fd69bb6904105b58b92fb48b54ce17ac9f685841
luifiller <luiz.fsilva@sptech.school>
2024-03-09 19:34:18 -0300

    feat: UI da index com componentes

--- app/src/main/java/com/example/techhub/MainActivity.kt
+++ app/src/main/java/com/example/techhub/MainActivity.kt
@@ -1,46 +1,195 @@
 package com.example.techhub
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
 import com.example.techhub.ui.theme.TechHubTheme
+import androidx.compose.ui.unit.sp
+import com.example.techhub.ui.theme.PrimaryBlue
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 class MainActivity : ComponentActivity() {
+    val primaryBlue = PrimaryBlue;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TechHubTheme {
-                // A surface container using the 'background' color from the theme
+                SetBarColor(color = Color.White)
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    IndexView()
                 }
             }
+            val toast: Toast = Toast.makeText(this, "Seja bem-vindo(a)!", Toast.LENGTH_SHORT)
+            toast.show()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+private fun SetBarColor(color: Color) {
+    val systemUiController = rememberSystemUiController()
+    SideEffect {
+        systemUiController.setStatusBarColor(color = color)
+    }
+}
+
+@Composable
+fun IndexView() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(horizontal = 24.dp, vertical = 16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        ImageSection()
+
+        CustomTextSection()
+
+        ButtonsSection()
+    }
+}
+@Composable
+fun ImageSection() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.mipmap.index_image),
+            contentDescription = "imagem de pessoas se cumprimentando",
+            contentScale = ContentScale.FillWidth,
+            modifier = Modifier
+                .width(252.dp)
+                .height(300.dp)
+                .fillMaxHeight()
+                .fillMaxWidth()
+        )
+    }
+}
+@Composable
+fun CustomTextSection() {
+    Column(
+        modifier = Modifier
+            .padding(vertical = 40.dp, horizontal = 10.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = buildAnnotatedString {
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.Black,
+                        fontWeight = FontWeight.Medium,
+                        fontSize = 32.sp
+                    )
+                ) {
+                    append("O ponto de encontro dos ")
+                }
+                withStyle(
+                    style = SpanStyle(
+                        color = Color(PrimaryBlue.value),
+                        fontWeight = FontWeight.Medium,
+                        fontSize = 32.sp
+                    )
+                ) {
+                    append("talentos")
+                }
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.Black,
+                        fontWeight = FontWeight.Medium,
+                        fontSize = 32.sp
+                    )
+                ) {
+                    append("!")
+                }
+            },
+            style = MaterialTheme.typography.titleLarge,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(bottom = 10.dp)
+        )
+
+        Text(
+            text = "Unindo talento e necessidade.",
+            color = Color.Black,
+            fontWeight = FontWeight.Thin,
+            fontSize = 16.sp
+        )
+    }
+}
+@Composable
+fun ButtonsSection() {
+    Column {
+        ElevatedButtonTH(
+            onClick = { /*TODO*/ },
+            text = "Entrar",
+            backgroundColor = Color(PrimaryBlue.value)
+        )
+
+        ElevatedButtonTH(
+            onClick = { /*TODO*/ },
+            text = "Cadastrar",
+            backgroundColor = Color(Color.White.value),
+            textColor = Color(PrimaryBlue.value)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun IndexViewPreview() {
     TechHubTheme {
-        Greeting("Android")
+        IndexView()
     }
 }
