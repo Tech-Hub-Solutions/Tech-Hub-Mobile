b78e6f6fe2a99409d1c58b58bc3fb00e81b3f724
luifiller <luiz.fsilva@sptech.school>
2024-03-09 19:29:40 -0300

    feat: UI da index com componentes

---
+++ app/src/main/java/com/example/techhub/ElevatedButtonTH.kt
@@ -0,0 +1,41 @@
+package com.example.techhub
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.techhub.ui.theme.PrimaryBlue
+
+@Composable
+fun ElevatedButtonTH(
+    onClick: () -> Unit,
+    text: String,
+    backgroundColor: Color,
+    textColor: Color = Color.White
+) {
+    ElevatedButton(
+        onClick = { onClick() },
+        modifier = Modifier
+            .padding(10.dp)
+            .width(350.dp)
+            .height(60.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = backgroundColor,
+            contentColor = textColor,
+        ),
+        border = BorderStroke(
+            1.dp, Color(PrimaryBlue.value)
+        ),
+        shape = RoundedCornerShape(10.dp),
+    ) {
+        Text(text = text)
+    }
+}
