add5bfad56e3d38be40e82423a86e14511ca3f12
luifiller <luiz.fsilva@sptech.school>
2024-03-10 19:42:47 -0300

    feat: add login com componentes e lógicas de navegação

---
+++ app/src/main/java/com/example/techhub/service/Navigation.kt
@@ -0,0 +1,27 @@
+package com.example.techhub.service
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.techhub.utils.Screen
+import com.example.techhub.view.IndexView
+import com.example.techhub.view.LoginView
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = Screen.IndexScreen.route) {
+        composable(route = Screen.IndexScreen.route) {
+            IndexView(navController = navController)
+        }
+        composable(route = Screen.LoginScreen.route) {
+            LoginView(navController = navController)
+        }
+        composable(route = Screen.CadastroScreen.route) {
+            // CadastroView()
+        }
+    }
+}
+
--- app/src/main/java/com/example/techhub/ui/theme/Color.kt
+++ app/src/main/java/com/example/techhub/ui/theme/Color.kt
@@ -6,6 +6,7 @@ val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 val PrimaryBlue = Color(0xFF0F9EEA)
+val GrayText = Color(0xFF505050)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
