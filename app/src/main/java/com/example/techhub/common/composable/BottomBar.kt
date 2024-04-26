db8238513c0000638eaad8d446df8b7bf725b2f1
MuriloSBarbosa <murilo.barbosa@sptech.school>
2024-04-25 21:00:30 -0300

    feat: add shadow to bottom bar

--- app/src/main/java/com/example/techhub/common/composable/BottomBar.kt
+++ app/src/main/java/com/example/techhub/common/composable/BottomBar.kt
@@ -1,13 +1,13 @@
 package com.example.techhub.common.composable
 
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.outlined.Sms
 import androidx.compose.material.icons.outlined.TravelExplore
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Icon
@@ -17,15 +17,28 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.example.techhub.common.utils.shadowCustom
+
 import com.example.techhub.presentation.ui.theme.PrimaryBlue
 
 @Composable
-fun BottomBar(isEmpresa: Boolean) {
+fun BottomBar(
+    isEmpresa: Boolean
+) {
     BottomAppBar(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(56.dp)
+            .shadowCustom(
+                color = Color.Black.copy(alpha = 0.4f),
+                offsetY = 4.dp,
+                blurRadius = 16.dp,
+                shapeRadius = 16.dp
+            ),
         actions = {
             Row(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxSize(),
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = androidx.compose.foundation.layout.Arrangement.SpaceEvenly,
             ) {
@@ -50,16 +63,6 @@ fun BottomBar(isEmpresa: Boolean) {
                                 .height(28.dp)
                         )
                     }
-                }
-                IconButton(onClick = { /* TODO - redirecionar para o chat */ }) {
-                    Icon(
-                        Icons.Outlined.Sms,
-                        contentDescription = "@string/btn_description_chat",
-                        tint = Color(PrimaryBlue.value),
-                        modifier = Modifier
-                            .width(28.dp)
-                            .height(28.dp)
-                    )
                 }
                 IconButton(onClick = { /* TODO - inserir a foto de perfil da pessoa + abrir o pop de editar perfil */ }) {
                     Icon(
