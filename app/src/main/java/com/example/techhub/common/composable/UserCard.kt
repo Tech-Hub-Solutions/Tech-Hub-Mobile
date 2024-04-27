3bda8101cfd9db647193f7960460b2aa9ee0597a
luifiller <luiz.fsilva@sptech.school>
2024-04-27 18:25:57 -0300

    fix: import LocalContext no UserCard

--- app/src/main/java/com/example/techhub/common/composable/UserCard.kt
+++ app/src/main/java/com/example/techhub/common/composable/UserCard.kt
@@ -29,6 +29,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
