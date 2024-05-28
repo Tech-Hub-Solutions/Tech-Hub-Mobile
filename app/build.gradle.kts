f23bc7084bc2c39e13113368457b20a718fe6f96
kaiosenaa <kaio.sena@sptech.school>
2024-05-28 20:32:47 -0300

    feat: Implementacao de funcao de notificao - Firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -97,4 +97,5 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
+    implementation("com.google.firebase:firebase-analytics-ktx")
 }
--- app/src/main/java/com/example/techhub/presentation/MainActivity.kt
+++ app/src/main/java/com/example/techhub/presentation/MainActivity.kt
@@ -6,7 +6,14 @@ import com.example.techhub.presentation.index.IndexActivity
 import com.example.techhub.presentation.perfil.PerfilActivity
 import android.content.Intent
 import android.net.Uri
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.compose.setContent
 import com.example.techhub.common.utils.verifyCredentials
+import com.google.android.gms.tasks.OnCanceledListener
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.FirebaseMessagingService
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,5 +27,20 @@ class MainActivity : ComponentActivity() {
         val appLinkIntent: Intent = intent
         val appLinkAction: String? = appLinkIntent.action
         val appLinkData: Uri? = appLinkIntent.data
+
+        FirebaseMessaging.getInstance().token
+            .addOnCompleteListener(OnCompleteListener{ task ->
+                if (!task.isSuccessful){
+                    Log.d("FCM Notify", "Fetching FCM registration token failed", task.exception)
+                    return@OnCompleteListener
+                }
+
+                // Obtendo novo token de registro
+                val FCMtoken: String? = task.result
+                Log.d("FCM Token", FCMtoken, task.exception)
+                Toast.makeText(this, FCMtoken, Toast.LENGTH_SHORT).show()
+            })
     }
+
+
 }
