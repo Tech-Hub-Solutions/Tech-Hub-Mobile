19491fc05cd7c5b4055aac5386dc20811f5d023f
luifiller <luiz.fsilva@sptech.school>
2024-06-04 19:23:16 -0300

    fix: InformacoesAdicionaisPerfil p/ strings.

--- app/src/main/java/com/example/techhub/common/objects/InformacoesAdicionaisPerfil.kt
+++
@@ -1,7 +0,0 @@
-package com.example.techhub.common.objects
-
-object InformacoesAdicionaisPerfil {
-    const val projetosFinalizados = "Projetos finalizados"
-    const val empresasInteressadas = "Empresas interessadas"
-    const val recomendacoes = "Recomendações"
-}
